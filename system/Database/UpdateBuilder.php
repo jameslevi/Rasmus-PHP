c443e4a8af0baf7cca08f300dd58c054bd6d323f
WIN-OUTDV3ITTIV\Administrator <nerdlabenterprise@gmail.com>
2020-08-11 22:11:26 +0800

    :star: Database UPDATE query builder.

--- system/Database/UpdateBuilder.php
+++ system/Database/UpdateBuilder.php
@@ -1,16 +1,113 @@
 <?php
 
-namespace Rasmus\Database;
+namespace Raccoon\Database;
+
+use Raccoon\Util\Str;
 
 class UpdateBuilder extends WhereBuilder
 {
-    protected $tablename;
-    protected $data = [];
+    /**
+     * Name of table to update.
+     */
 
-    public function __construct(string $tablename, array $data = [])
+    protected $tablename;
+
+    /**
+     * Data to update.
+     */
+
+    protected $data = [
+
+        'set' => [],
+
+        'where' => [
+
+            'enable' => false,
+
+            'raw' => null,
+
+            'items' => [],
+
+        ],
+
+    ];
+
+    public function __construct(string $tablename, array $data)
     {
         $this->tablename = $tablename;
-        $this->data = $data;
+        $this->data['set'] = $data;
+    }
+
+    /**
+     * Return UPDATE query sql.
+     */
+
+    public function sql()
+    {
+        $sql = 'UPDATE ' . $this->tablename . ' SET ';
+
+        if(!empty($this->data['set']))
+        {
+            foreach($this->data['set'] as $key => $val)
+            {
+                if(is_string($val))
+                {
+                    $sql .= $key . " = '" . DB::sanitize($val) . "', ";
+                }
+                else if(is_int($val) || is_bool($val))
+                {
+                    $sql .= $key . " = " . DB::sanitize($val) . ", ";
+                }
+            }
+
+            if(Str::endWith($sql, ', '))
+            {
+                $sql = Str::move($sql, 0, 2) . ' ';
+            }
+        }
+
+        $where = $this->data['where'];
+
+        if(!is_null($where['raw']))
+        {
+            $sql .= 'WHERE ' . $this->data['where']['raw'] . ' ';
+        }
+        else if(!empty($where['items']))
+        {
+            $sql .= 'WHERE ';
+
+            foreach($where['items'] as $data)
+            {
+                if(!$where['enable'])
+                {
+                    $where['enable'] = true;
+                    $this->data['where']['enable'] = true;
+
+                    if(Str::startWith($data, 'AND '))
+                    {
+                        $data = Str::move($data, 4);
+                    }
+                }
+
+                $sql .= $data;
+            }
+        }
+
+        if(Str::endWith($sql, ' '))
+        {
+            $sql = Str::move($sql, 0, 1);
+        }
+
+        return $sql;
+    }
+
+    /**
+     * Execute UPDATE query.
+     */
+
+    public function exec()
+    {
+        return DB::query($this->sql());
     }
 
 }
