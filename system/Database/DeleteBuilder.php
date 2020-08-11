9277a7a743fd07e1e12c7c2110ecfbca16860657
WIN-OUTDV3ITTIV\Administrator <nerdlabenterprise@gmail.com>
2020-08-11 23:06:01 +0800

    :star: Database DELETE query builder.

--- system/Database/DeleteBuilder.php
+++ system/Database/DeleteBuilder.php
@@ -2,12 +2,85 @@
 
 namespace Raccoon\Database;
 
+use Raccoon\Util\Str;
+
 class DeleteBuilder extends WhereBuilder
 {
+    /**
+     * Table to execute delete query.
+     */
+
+    private $tablename;
+
+    protected $data = [
+
+        'where' => [
+
+            'enable' => false,
+
+            'raw' => null,
+
+            'items' => [],
+
+        ],
+
+    ];
 
     public function __construct(string $tablename)
     {
-        
+        $this->tablename = $tablename;
+    }
+
+    /**
+     * Return DELETE sql query.
+     */
+
+    public function sql()
+    {
+        $sql = 'DELETE FROM ' . $this->tablename . ' ';
+
+        $where = $this->data['where'];
+
+        if(!is_null($where['raw']))
+        {
+            $sql .= 'WHERE ' . $this->data['where']['raw'] . ' ';
+        }
+        else if(!empty($where['items']))
+        {
+            $sql .= 'WHERE ';
+
+            foreach($where['items'] as $data)
+            {
+                if(!$where['enable'])
+                {
+                    $where['enable'] = true;
+                    $this->data['where']['enable'] = true;
+
+                    if(Str::startWith($data, 'AND '))
+                    {
+                        $data = Str::move($data, 4);
+                    }
+                }
+
+                $sql .= $data;
+            }
+        }
+
+        if(Str::endWith($sql, ' '))
+        {
+            $sql = Str::move($sql, 0, 1);
+        }
+
+        return $sql;
+    }
+
+    /**
+     * Execute DELETE query.
+     */
+
+    public function exec()
+    {
+        return DB::query($this->sql());
     }
 
 }
