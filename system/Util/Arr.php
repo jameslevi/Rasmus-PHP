2047ad6a8ba57526f6d7935195187801cb3052ed
WIN-OUTDV3ITTIV\Administrator <nerdlabenterprise@gmail.com>
2020-08-17 15:53:08 +0800

    :star: Array utility sort values.

--- system/Util/Arr.php
+++ system/Util/Arr.php
@@ -5,6 +5,40 @@ namespace Raccoon\Util;
 class Arr
 {
     /**
+     * Sort array in descending order.
+     */
+
+    public static function desc(array $array, bool $byKey = false)
+    {
+        if($byKey)
+        {
+            krsort($array);
+        }
+        else
+        {
+            rsort($array);
+        }
+        return $array;
+    }
+
+    /**
+     * Sort array in ascending order.
+     */
+
+    public static function asc(array $array, bool $byKey = false)
+    {
+        if($byKey)
+        {
+            ksort($array);
+        }
+        else
+        {
+            asort($array);
+        }
+        return $array;
+    }
+
+    /**
      * Test if array is multidimensional or not.
      */
 
